24323a3fad8cd67200f8ac60e7e2c0b4ddc4236f
Chidinma Orajiaku <corajiaku96@gmail.com>
2017-08-10 09:02:29 +0100

    Removed server directory from gh-pages branch

--- Server/server/controllers/index.js
+++
@@ -1,9 +0,0 @@
-import users from './users';
-import books from './books';
-// const users = require('./users');
-// const books = require('./books');
-console.log(users);
-export default {
-  users,
-  books
-};
