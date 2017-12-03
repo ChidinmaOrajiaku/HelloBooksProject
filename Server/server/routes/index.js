4488f6d1f7693045d34329b54094cb0d38497839
ChidinmaOrajiaku <corajiaku96@gmail.com>
2017-12-03 19:53:05 +0100

    refactor(controllers): refactor error messages

--- Server/server/routes/index.js
+++ Server/server/routes/index.js
@@ -14,22 +14,22 @@ export default (app) => {
    */
   app.post('/api/v1/users/signup', usersController.create);
   app.post('/api/v1/users/signin', usersController.login);
-  app.put('/api/v1/users/:usersId', authenticate.verifyUser, usersController.updatePassword);
+  app.put('/api/v1/users/:userId', authenticate.verifyUser, usersController.updatePassword);
   app.get('/api/v1/users/books', authenticate.verifyUser, booksController.list);
   app.get('/api/v1/books/:id', authenticate.verifyUser, booksController.listABook);
-  app.get('/api/v1/users/:usersId', authenticate.verifyUser, usersController.getUser);
-  app.get('/api/v1/users/:usersId/history', authenticate.verifyUser, booksController.listAllBooksBorrowed);
+  app.get('/api/v1/users/:userId', authenticate.verifyUser, usersController.getUser);
+  app.get('/api/v1/users/:userId/history', authenticate.verifyUser, booksController.listAllBooksBorrowed);
   app.post('/api/v1/users/:userId/books', authenticate.verifyUser, booksController.borrow);
-  app.get('/api/v1/users/:usersId/books', authenticate.verifyUser, booksController.listNotReturnedBooks);
-  app.put('/api/v1/users/:usersId/books', authenticate.verifyUser, booksController.returnBooks);
+  app.get('/api/v1/users/:userId/books', authenticate.verifyUser, booksController.listNotReturnedBooks);
+  app.put('/api/v1/users/:userId/books', authenticate.verifyUser, booksController.returnBooks);
 
   /**
    * Admin API
    */
   app.get('/api/v1/users', authenticate.verifyUser, authenticate.verifyAdmin, usersController.adminCountAllUser);
   app.post('/api/v1/users/books', authenticate.verifyUser, authenticate.verifyAdmin, booksController.create);
-  app.put('/api/v1/books/:booksId', authenticate.verifyUser, authenticate.verifyAdmin, booksController.update);
-  app.delete('/api/v1/books/:booksId', authenticate.verifyUser, authenticate.verifyAdmin, booksController.deleteBook);
+  app.put('/api/v1/books/:bookId', authenticate.verifyUser, authenticate.verifyAdmin, booksController.update);
+  app.delete('/api/v1/books/:bookId', authenticate.verifyUser, authenticate.verifyAdmin, booksController.deleteBook);
   app.get('/api/v1/books', authenticate.verifyUser, authenticate.verifyAdmin, booksController.adminCountAllBooks);
   app.get('/api/v1/users/books/history', authenticate.verifyUser, authenticate.verifyAdmin, booksController.adminCountAllRentedBooks);
   app.get('/api/v1/users/books/unreturned/history', authenticate.verifyUser, authenticate.verifyAdmin, booksController.adminCountAllNotReturnedBooks);
